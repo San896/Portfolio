65170518a03268a4c8712493616f0aef22f0d7bd
san896 <sannlfernandez8@gmail.com>
2022-12-15 23:39:03 -0300

    commit

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
+import style from './About.module.css'
+import foto from '../images/foto.png'
 
 function AboutMe() {
     return (
-        <div>
-           <p>
-            Mi nombre es Santiago, tengo 26 años, soy un Desarrolador Web Full Stack Jr. Me instrui en el bootcamp de SoyHenry, un curso intensivo con +800 hrs de practica y muchos proyectos. Me encanto la programacion y es a lo que me quiero dedicar.
-             Como Full-Stack me desenvuelvo bien tanto en Front-End, Back-End como en manejo de Base de Datos, no tengo ninguna preferencia al momento de elegir. Soy bueno resolviendo problemas, encarando desafios nuevos, trabajando en equipo con metodologias agiles como SCRUM. Me encantan los animales, tengo como hobbies hacer ejercicio y jugar al futbol con amigos, me considero una persona paciente y con mucha resiliencia . Actualmente me ecnuentro trabajando en algunos proyectos, estudiando tecnologias nuevas y mejorando las aprendidas. Estoy en busca de empleo IT, preferentemente remoto pero escucho cualquier oferta
-            </p> 
-        </div>
+
+<div class="card mb-3" style={{width: '85rem', backgroundColor:'transparent', display: 'flex', position: 'absolute', left:'9rem', top:'10rem'}}>
+
+<div class="row g-0">
+
+  <div class="col-md-8">
+
+    <div class="card-body">
+      <h2 class="card-title">About Me</h2>
+      <p class="card-text">Mi nombre es Santiago, tengo 26 años, soy un Desarrolador Web Full Stack Jr. Me instrui en el bootcamp de SoyHenry, un curso intensivo con +800 hrs de practica y muchos proyectos. Me encanto la programacion y es a lo que me quiero dedicar.<br/>
+          Como Full-Stack me desenvuelvo bien tanto en Front-End, Back-End como en manejo de Base de Datos, no tengo ninguna preferencia al momento de elegir. Soy bueno resolviendo problemas, encarando desafios nuevos, trabajando en equipo con metodologias agiles como SCRUM. Me encantan los animales, tengo como hobbies hacer ejercicio y jugar al futbol con amigos, me considero una persona paciente y con mucha resiliencia . Actualmente me ecnuentro trabajando en algunos proyectos, estudiando tecnologias nuevas y mejorando las aprendidas. Estoy en busca de empleo IT, preferentemente remoto pero escucho cualquier oferta.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+    
+  </div>
+    <div class="col-md-4">
+    <img src= { foto }  alt="..." style={{width:'20rem', height:'20rem', borderRadius:'999px'}}/>
+  </div>
+</div>
+</div>
     );
 }
 
--- src/components/Exp.jsx
+++ src/components/Exp.jsx
@@ -1,15 +1,32 @@
-import React from 'react';
+import React from "react";
 
 function Exp(props) {
-    return (
-        <div>
-            <div>Soy Henry</div>
-            <div> Bootcamp intensivo, +800 hrs de practivca- tecnologias trabajadas : Javascript | SQL | React | Redux | HTML | Bootstrap | CSS | Node.js | Express | Sequelize | PostgreSQL | SQLite | Scrum | GIT - GIT-flow | Material UI UX.
-                Proyecto individual
-                Proyecto colaborativo
-            </div>
-        </div>
-    );
+  return (
+    <div class="card text-center" style={{display:'flex', width:'70rem', left:'9rem', top:'9rem', backgroundColor:'transparent'}}>
+
+      <div class="card-header">Experience</div>
+
+      <div class="card-body">
+
+        <h5 class="card-title">SoyHenry Bootcamp</h5>
+
+        <div class="card-footer text-muted">October 2021 - November 2022</div>
+        <p class="card-text">
+          Bootcamp intensivo, +800 hrs de practica y clases. Enfocado en
+          desarrolo Web Full Stack. Tecnologias trabajadas : Javascript | SQL |
+          React | Redux | HTML | Bootstrap | CSS | Node.js | Express | Sequelize
+          | PostgreSQL | SQLite | Scrum | GIT - GIT-flow | Material UI UX.{" "}
+          <br />
+          Particiapacion en proyectos individuales y grupales. Te invito a que
+          veas mis proyectos
+        </p>
+
+        <a href="/proyects" class="btn btn-primary">
+          Ver Proyectos
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default Exp;
