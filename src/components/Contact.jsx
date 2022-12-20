b62d8f08254f88b53f8bb7b4bdf1aac253dc6b6f
san896 <sannlfernandez8@gmail.com>
2022-12-19 21:18:31 -0300

    oleoleola

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,11 +1,40 @@
-import React from 'react';
+import React from "react";
+import { useForm, ValidationError } from '@formspree/react';
 
 function Contact() {
+    const [state, handleSubmit] = useForm("xjvdbwpo");
+    if (state.succeeded) {
+        return <p>Thanks, i will contatc you briefly</p>;
+    }
     return (
-        <div>
-            <p>Formulario de contacto</p>
-        </div>
+        <form onSubmit={handleSubmit}>
+        <label htmlFor="email">
+          Email Address
+        </label>
+        <input
+          id="email"
+          type="email" 
+          name="email"
+        />
+        <ValidationError 
+          prefix="Email" 
+          field="email"
+          errors={state.errors}
+        />
+        <textarea
+          id="message"
+          name="message"
+        />
+        <ValidationError 
+          prefix="Message" 
+          field="message"
+          errors={state.errors}
+        />
+        <button type="submit" disabled={state.submitting}>
+          Submit
+        </button>
+      </form>
     );
-}
+  }
 
 export default Contact;
